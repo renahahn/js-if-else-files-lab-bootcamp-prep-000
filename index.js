10342279364fef5a71e60dfef0c8e1cd78effd70
Rena Hahn <rhahn@bu.edu>
2019-02-05 19:56:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,13 +2,6 @@ var name = "Joe"
 var height = "74"
 
 
-var tests = require("./tests.js")
-
-console.log(tests.name)
-
-var tests = require("./tests.js")
-
-console.log(tests.height)
 
 // Don't worry about this
 module.exports = { name, height
